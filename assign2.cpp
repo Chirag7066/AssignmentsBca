f43657dc63d25d4f6500e3330a34411111a3c306
Chirag7066 <chirag7066.ca23@chitkara.edu.in>
2024-02-28 20:24:38 +0530

    Guess the number

---
+++ assign2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std; //CHANGE according to assignment :)
+class game{
+    int computerGeneratedNumber;
+    int userGuessedNumber=0, attempts=1;
+public:
+    void generateRandomNumber(){
+        computerGeneratedNumber=rand() % 100 +1;
+    }
+    void startGame(){
+    generateRandomNumber();
+    do{
+        cout<<"Guess the number: ";
+        cin>>userGuessedNumber;
+        if (computerGeneratedNumber>userGuessedNumber){
+            cout<<"Attempt: "<<attempts<<endl;
+            cout<<"Message: Guessed number is smaller than computer generated number"<<endl;
+        }else if(computerGeneratedNumber<userGuessedNumber){
+            cout<<"Attempt: "<<attempts<<endl;
+            cout<<"Guessed number is greater than computer generated number"<<endl;
+        }else{
+            cout<<"Attempt: "<<attempts<<endl;
+            cout<<"Message: Congratulation you won the game in "<<attempts<<" attempts"<<endl;
+        }
+        attempts++;
+    } while (computerGeneratedNumber!=userGuessedNumber);
+    
+    // while(computerGeneratedNumber!=userGuessedNumber){
+    //     cout<<"Guess the number: ";
+    //     cin>>userGuessedNumber;
+    //     if (computerGeneratedNumber>userGuessedNumber){
+    //         cout<<"Attempt: "<<attempts<<endl;
+    //         cout<<"Message: Guessed number is smaller than computer generated number"<<endl;
+    //     }else if(computerGeneratedNumber<userGuessedNumber){
+    //         cout<<"Attempt: "<<attempts<<endl;
+    //         cout<<"Guessed number is greater than computer generated number"<<endl;
+    //     }else{
+    //         cout<<"Attempt: "<<attempts<<endl;
+    //         cout<<"Message: Congratulation you won the game in "<<attempts<<" attempts"<<endl;
+    //     }
+    //     attempts++;
+    // }
+    }
+};
+int main(){
+    game g1;
+    g1.startGame();
+    return 0;
+}
